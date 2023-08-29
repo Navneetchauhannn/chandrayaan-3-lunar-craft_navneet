c7e8b72a25a55d125665f6a8f31442e2774ce1e4
Navneetchauhannn <cn5508119@gmail.com>
2023-08-30 02:36:37 +0530

    moving craft forward and backward

--- src/main/java/spacecraft/SpaceCraftUtils.java
+++ src/main/java/spacecraft/SpaceCraftUtils.java
@@ -1,27 +1,26 @@
 package spacecraft;
 
-
 enum Direction {
     N, S, E, W, Up, Down
 }
 
 public class SpaceCraftUtils {
-	
-	private static final int GALACTIC_UPPER_BOUND = 100;
+
+    private static final int GALACTIC_UPPER_BOUND = 100;
     private static final int GALACTIC_LOWER_BOUND = -100;
     private static final int GALACTIC_RIGHT_BOUND = 100;
     private static final int GALACTIC_LEFT_BOUND = -100;
-    
-	private int x, y, z;
-	private Direction direction;
-	
-	public SpaceCraftUtils(int x, int y, int z, Direction direction) {
+
+    private int x, y, z;
+    protected Direction direction;
+
+    public SpaceCraftUtils(int x, int y, int z, Direction direction) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.direction = direction;
     }
-	
+
     public int getX() {
         return x;
     }
@@ -38,5 +37,84 @@ public class SpaceCraftUtils {
         return direction;
     }
 
-	
+    public void moveForward() {
+        switch (direction) {
+            case N:
+                if (y + 1 <= GALACTIC_UPPER_BOUND) {
+                    y += 1;
+                }
+                break;
+            case S:
+                if (y - 1 >= GALACTIC_LOWER_BOUND) {
+                    y -= 1;
+                }
+                break;
+            case E:
+                if (x + 1 <= GALACTIC_RIGHT_BOUND) {
+                    x += 1;
+                }
+                break;
+            case W:
+                if (x - 1 >= GALACTIC_LEFT_BOUND) {
+                    x -= 1;
+                }
+                break;
+            case Up:
+                if (z + 1 <= GALACTIC_UPPER_BOUND) {
+                    z += 1;
+                }
+                break;
+            case Down:
+                if (z - 1 >= GALACTIC_LOWER_BOUND) {
+                    z -= 1;
+                }
+                break;
+        }
+    }
+
+    public void moveBackward() {
+        switch (direction) {
+            case N:
+                if (y - 1 >= GALACTIC_LOWER_BOUND) {
+                    y -= 1;
+                }
+                break;
+            case S:
+                if (y + 1 <= GALACTIC_UPPER_BOUND) {
+                    y += 1;
+                }
+                break;
+            case E:
+                if (x - 1 >= GALACTIC_LEFT_BOUND) {
+                    x -= 1;
+                }
+                break;
+            case W:
+                if (x + 1 <= GALACTIC_RIGHT_BOUND) {
+                    x += 1;
+                }
+                break;
+            case Up:
+                if (z - 1 >= GALACTIC_LOWER_BOUND) {
+                    z -= 1;
+                }
+                break;
+            case Down:
+                if (z + 1 <= GALACTIC_UPPER_BOUND) {
+                    z += 1;
+                }
+                break;
+        }
+    }
+
+    public void executeCommands(String[] commands) {
+        for (String command : commands) {
+            if (command == "f") {
+                moveForward();
+            } else {
+                moveBackward();
+            }
+        }
+    }
+
 }
