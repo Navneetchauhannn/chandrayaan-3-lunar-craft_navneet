ee8e869b0836dc0aa0611242e75e2ff2deba8f20
Navneetchauhannn <cn5508119@gmail.com>
2023-08-30 10:44:02 +0530

    turn right and left

--- src/main/java/spacecraft/SpaceCraftUtils.java
+++ src/main/java/spacecraft/SpaceCraftUtils.java
@@ -1,26 +1,28 @@
 package spacecraft;
 
+
 enum Direction {
     N, S, E, W, Up, Down
 }
 
 public class SpaceCraftUtils {
-
-    private static final int GALACTIC_UPPER_BOUND = 100;
+	
+	private static final int GALACTIC_UPPER_BOUND = 100;
     private static final int GALACTIC_LOWER_BOUND = -100;
     private static final int GALACTIC_RIGHT_BOUND = 100;
     private static final int GALACTIC_LEFT_BOUND = -100;
-
-    private int x, y, z;
-    protected Direction direction;
-
-    public SpaceCraftUtils(int x, int y, int z, Direction direction) {
+    
+	private int x, y, z;
+	protected Direction direction;
+	protected Direction prevDirection = Direction.W;
+	
+	public SpaceCraftUtils(int x, int y, int z, Direction direction) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.direction = direction;
     }
-
+	
     public int getX() {
         return x;
     }
@@ -36,7 +38,7 @@ public class SpaceCraftUtils {
     public Direction getDirection() {
         return direction;
     }
-
+    
     public void moveForward() {
         switch (direction) {
             case N:
@@ -106,15 +108,87 @@ public class SpaceCraftUtils {
                 break;
         }
     }
+    
+    public void turnLeft() {
+        switch (direction) {
+            case N:
+                direction = Direction.W;
+                break;
+            case S:
+                direction = Direction.E;
+                break;
+            case E:
+                direction = Direction.N;
+                break;
+            case W:
+                direction = Direction.S;
+                break;
+            case Up:
+            case Down:
+            	if(prevDirection!=null) {
+            		if(prevDirection==Direction.N) {
+                		direction = Direction.W;
+                	} else if(prevDirection==Direction.E) {
+                		direction = Direction.N;
+                	} else if(prevDirection==Direction.W) {
+                		direction = Direction.S;
+                	} else if(prevDirection==Direction.S) {
+                		direction = Direction.E;
+                	}
+            	}
+            	break;
+			default:
+				break;
+	        }
+    }
 
-    public void executeCommands(String[] commands) {
-        for (String command : commands) {
-            if (command == "f") {
-                moveForward();
-            } else {
-                moveBackward();
-            }
+    public void turnRight() {
+        switch (direction) {
+            case N:
+                direction = Direction.E;
+                break;
+            case S:
+                direction = Direction.W;
+                break;
+            case E:
+                direction = Direction.S;
+                break;
+            case W:
+                direction = Direction.N;
+                break;
+            case Up:
+            case Down:
+            	if(prevDirection!=null) {
+            		if(prevDirection==Direction.N) {
+                		direction = Direction.E;
+                	} else if(prevDirection==Direction.E) {
+                		direction = Direction.S;
+                	} else if(prevDirection==Direction.S) {
+                		direction = Direction.W;
+                	} else if(prevDirection==Direction.W) {
+                		direction = Direction.N;
+                	}
+            	}
+            	break;
+            default:
+				break;
         }
     }
 
+    
+    public void executeCommands(String[] commands) {
+        for (String command : commands) {
+        	if(command=="f") {
+        		moveForward();
+        	} else if(command=="b") {
+        		moveBackward();
+        	} else if(command=="l") {
+        		turnLeft();
+        	} else if(command=="r") {
+        		turnRight();
+        	}
+        }
+    }
+
+	
 }
--- src/test/java/spacecraft/SpaceCraftUtilsTest.java
+++ src/test/java/spacecraft/SpaceCraftUtilsTest.java
@@ -7,8 +7,61 @@ import org.junit.jupiter.api.Test;
 class SpaceCraftUtilsTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	void move() {
+		SpaceCraftUtils craft = new SpaceCraftUtils(0, 0, 0, Direction.N);
+		
+        String[] commands = {"f", "f", "b", "b", "b"};
+        craft.executeCommands(commands);
 
+        assertEquals(-1, craft.getY(), "y coordinate is not ture");
+        
+        // checking for directions
+        craft = new SpaceCraftUtils(0, 0, 0, Direction.S);
+        craft.executeCommands(commands);
+        
+        assertEquals(1, craft.getY(), "y coordinate is not ture");
+        
+
+        craft = new SpaceCraftUtils(0, 0, 0, Direction.E);
+        craft.executeCommands(commands);
+        
+        assertEquals(-1, craft.getX(), "x coordinate is not ture");
+        
+        craft = new SpaceCraftUtils(0, 0, 0, Direction.W);
+        craft.executeCommands(commands);
+        
+        assertEquals(1, craft.getX(), "x coordinate is not ture");
+        
+        craft = new SpaceCraftUtils(0, 0, 0, Direction.Up);
+        craft.executeCommands(commands);
+        
+        assertEquals(-1, craft.getZ(), "z coordinate is not ture");
+        
+        craft = new SpaceCraftUtils(0, 0, 0, Direction.Down);
+        craft.executeCommands(commands);
+        
+        assertEquals(1, craft.getZ(), "z coordinate is not ture");
+        
+        // checking for positions
+        craft = new SpaceCraftUtils(100, 100, 100, Direction.N);
+        craft.executeCommands(commands);
+
+        assertEquals(97, craft.getY(), "y coordinate is not ture");
+	}
+	
+	@Test	
+	void turnLeftRight() {
+		SpaceCraftUtils craft = new SpaceCraftUtils(0, 0, 0, Direction.Down);
+		
+		 String[] commands = {"r", "l", "r"};
+		 craft.executeCommands(commands);
+		 
+		 assertEquals(Direction.N, craft.getDirection(), "not turned");
+		 
+		 craft = new SpaceCraftUtils(0, 0, 0, Direction.E);
+
+		 craft.executeCommands(commands);
+		 
+		 assertEquals(Direction.S, craft.getDirection(), "not turned");
+	}
 }
